8ef5371bf8012c5607748c099e2f37503e537890
Adrian <serbanedward@yahoo.com>
2024-10-30 21:04:13 +0100

    update new

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoute from "./routes/userRoutes.js";
+import User from "./models/userModel.js";
+//import userRoute from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -18,7 +19,18 @@ app.get("/", (req, res) => {
   res.send("Deployment succesfully");
 });
 
-app.use("/api/submit", userRoute);
+//app.use("/api/submit", userRoute);
+app.post("api/submit", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ details: [user] });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
